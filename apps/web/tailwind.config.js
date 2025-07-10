5b35dbc59f9634ec4f75c3272ec81fa7ff69c38a
junggwanhun <ghjung@vpplab.kr>
2025-07-10 16:39:50 +0900

    fix: tailwind.config.js 수정

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
       '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
     ),
     ...createGlobPatternsForDependencies(__dirname),
+    join(__dirname, '../../shared-ui/src/**/*.{js,ts,jsx,tsx}'),
   ],
   theme: {
     extend: {
