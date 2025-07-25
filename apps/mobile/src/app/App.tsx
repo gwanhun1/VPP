51d943b2cfa0415957508ea0b4020b0ac4acf72c
junggwanhun <ghjung@vpplab.kr>
2025-07-25 09:21:21 +0900

    fix: 개발환경 간편화

--- apps/mobile/src/app/App.tsx
+++ apps/mobile/src/app/App.tsx
@@ -1,10 +1,20 @@
-import { WebView } from 'react-native-webview';
+import React from 'react';
+import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
+
 export const App = () => {
   return (
-    <>
-      <WebView source={{ uri: 'https://vppweb.vercel.app/' }} />
-    </>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.text}>123</Text>
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  safeArea: { flex: 1, backgroundColor: '#fff' },
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  text: { fontSize: 32, color: '#14287f' }, // 메인 컬러 사용
+});
 
 export default App;
